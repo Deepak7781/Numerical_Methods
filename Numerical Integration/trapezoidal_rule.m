f15de208bd1df12a4f5426e63d9143c029dac777
Deepak7781 <deepak21082005@gmail.com>
2025-02-23 18:37:57 +0530

    added numerical_integrations

---
+++ Numerical Integration/trapezoidal_rule.m
@@ -0,0 +1,20 @@
+% Trapezoidal rule for single integration
+
+
+x = input("Enter the values of x as a matrix :");
+
+func = input('Enter a function in terms of x :','s');
+func = strrep(func, '^', '.^');
+func = strrep(func, '/', './');
+
+y = str2func(['@(x)', func]);
+
+n = length(x);
+
+h = x(2)-x(1); %assuming evenly spaced intervals
+
+y_values = y(x);
+
+integration_res = (h/2) * (y_values(1)+y_values(n) + 2*sum(y_values(2:n-1)));
+
+fprintf('Result = %.4f',integration_res);
